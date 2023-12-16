5cb42a03592adf1859aac5f498028afe264c3ff7
pvd232 <50126818+pvd232@users.noreply.github.com>
2023-12-16 13:19:55 -0500

    optimized moveToFront so entire list traversal unnecessary

--- comp/MoveToFront.java
+++ comp/MoveToFront.java
@@ -12,14 +12,41 @@ public class MoveToFront {
             chars[i] = i;
         while (!BinaryStdIn.isEmpty()) {
             char c = BinaryStdIn.readChar();
-            BinaryStdOut.write(chars[c]);
-            int i = 0;
-            while (i < c)
-                chars[i++] = i;
-            chars[i] = 0;
+            int idx = 0, prev = chars[idx], curr = chars[idx + 1];
+            if (prev != c)
+                for (int i = 1; i < chars.length; i++) {
+                    curr = chars[i];
+                    chars[i] = prev;
+                    prev = curr;
+                    if (prev == c) {
+                        idx = i;
+                        break;
+                    }
+                }
+            BinaryStdOut.write((char) idx);
+            chars[0] = c;
         }
         BinaryStdOut.flush();
     }
+    // old method, required traversing through entire chars array
+    // public static void encode() {
+    // int[] chars = new int[256];
+    // for (int i = 0; i < chars.length; i++)
+    // chars[i] = i;
+    // while (!BinaryStdIn.isEmpty()) {
+    // char c = BinaryStdIn.readChar();
+    // BinaryStdOut.write((char) chars[c]);
+
+    // int i = 0;
+    // while (i < chars.length) {
+    // if (chars[i] < chars[c])
+    // chars[i] = chars[i] + 1;
+    // i++;
+    // }
+    // chars[c] = 0;
+    // }
+    // BinaryStdOut.flush();
+    // }
 
     // apply move-to-front decoding, reading from standard input and writing to
     // standard output
