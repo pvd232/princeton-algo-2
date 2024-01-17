a9e890bdc5f896eed648d7c4a4666c753bf01b2c
pvd232 <50126818+pvd232@users.noreply.github.com>
2024-01-16 19:06:57 -0600

    1.34x ref for pair of verts. grade of 100. implemented caching of distTo and marked for v and w

--- graph/SAP.java
+++ graph/SAP.java
@@ -1,79 +1,93 @@
 package graph;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Queue;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class SAP {
     private final Digraph g;
     private final int n;
+    private final int[] distToV;
+    private final int[] distToW;
+    private final boolean[] markedV;
+    private final boolean[] markedW;
 
     public SAP(Digraph G) {
         if (G == null)
             throw new IllegalArgumentException();
         g = new Digraph(G);
         n = g.V();
+        distToV = new int[n];
+        distToW = new int[n];
+        markedV = new boolean[n];
+        markedW = new boolean[n];
     }
 
-    private void bfs(int[] distTo, boolean[] marked, Iterable<Integer> s) {
+    private void bfs(Iterable<Integer> s, Stack<Integer> changed) {
         Queue<Integer> q = new Queue<Integer>();
+
         for (int next : s) {
             q.enqueue(next);
-            marked[next] = true;
-            distTo[next] = 0;
+            changed.push(next);
+            markedV[next] = true;
         }
         while (!q.isEmpty()) {
             int v = q.dequeue();
-            for (int w : g.adj(v)) {
-                if (!marked[w]) {
+            for (int w : g.adj(v))
+                if (!markedV[w]) {
                     q.enqueue(w);
-                    marked[w] = true;
-                    distTo[w] = distTo[v] + 1;
+                    changed.push(w);
+                    markedV[w] = true;
+                    distToV[w] = distToV[v] + 1;
                 }
-            }
         }
     }
 
     // Finds the shortest ancestral path of vertices in v and w shortest path arrays
     private int pathTo(Iterable<Integer> v, Iterable<Integer> w, boolean length) {
-        int[] distToV = new int[n], distToW = new int[n];
-        boolean[] markedV = new boolean[n], markedW = new boolean[n];
-        bfs(distToV, markedV, v);
-        bfs(distToW, markedW, w);
-        return sp(distToV, distToW, markedV, markedW, v, w, length);
+        Stack<Integer> changedV = new Stack<>(), changedW = new Stack<>();
+        bfs(v, changedV);
+        int res = sp(w, length, changedW);
+
+        // Cleanup
+        while (!changedV.isEmpty()) {
+            int curr = changedV.pop();
+            markedV[curr] = false;
+            distToV[curr] = 0;
+        }
+        while (!changedW.isEmpty()) {
+            int curr = changedW.pop();
+            markedW[curr] = false;
+            distToW[curr] = 0;
+        }
+        return res;
     }
 
-    private int sp(int[] distToV, int[] distToW, boolean[] markedV, boolean[] markedW,
-            Iterable<Integer> v, Iterable<Integer> w, boolean length) {
-        boolean[] marked = new boolean[n];
+    private int sp(Iterable<Integer> w, boolean length, Stack<Integer> changed) {
         Queue<Integer> q = new Queue<>();
-
-        // Start search at v and w to avoid exploring unreachable vertices
-        for (int vert : v) {
-            q.enqueue(vert);
-            marked[vert] = true;
-        }
         for (int vert : w) {
             q.enqueue(vert);
-            marked[vert] = true;
+            changed.push(vert);
+            markedW[vert] = true;
         }
         int dist = -1, ancestor = -1;
         while (!q.isEmpty()) { // For each vertex, check for directed path to v and w
             int curr = q.dequeue();
             for (int adj : g.adj(curr)) {
-                if (!marked[adj]) {
-                    int adjDist = distToV[adj] + distToW[adj];
-                    if (dist == -1 || adjDist < dist) { // Prune, only pursuing adj with shorter dist
-                        q.enqueue(adj);
-                        marked[adj] = true;
-                    }
+                if (!markedW[adj]) {
+                    q.enqueue(adj);
+                    changed.push(adj);
+                    markedW[adj] = true;
+                    distToW[adj] = distToW[curr] + 1;
                 }
             }
-            if (markedV[curr] && markedW[curr]) { // If vertex is reachable from v and w it's an ancestor
+            if (markedV[curr]) { // If vertex is reachable from v and w it's an ancestor
                 int tmpDist = distToV[curr] + distToW[curr];
                 if (dist == -1 || tmpDist < dist) { // If dist unset, or curr dist < dist, update
                     dist = tmpDist;
@@ -142,30 +156,76 @@ public class SAP {
 
     // do unit testing of this class
     public static void main(String[] args) {
-        In in = new In(args[0]);
-        Digraph G = new Digraph(in);
-        SAP sap = new SAP(G);
-        while (!StdIn.isEmpty()) {
-            int v = StdIn.readInt();
-            int w = StdIn.readInt();
-            int length = sap.length(v, w);
-            int ancestor = sap.ancestor(v, w);
 
-            if (v == 3 && w == 11) {
-                assert length == 4;
-                assert ancestor == 1;
-            } else if (v == 9 && w == 12) {
-                assert length == 3;
-                assert ancestor == 5;
-            } else if (v == 7 && w == 2) {
-                assert length == 4;
-                assert ancestor == 0;
-            } else if (v == 1 && w == 6) {
-                assert length == -1;
-                assert ancestor == -1;
+        // Unit testing data
+        In testLength = new In("./tests/length.txt"), testAncestor = new In("./tests/ancestor.txt");
+        In synIn = new In("./inputs/synsets.txt"), hyperIn = new In("./inputs/hypernyms.txt");
+
+        HashMap<String, ArrayList<Integer>> nouns = new HashMap<>(); // List of nouns with associated synsets
+
+        ArrayList<String> s = new ArrayList<>(); // List of synsets
+
+        // Synset-indexed array of corresponding hypernyms
+        ArrayList<Bag<Integer>> adj = new ArrayList<>();
+
+        int lineNum = 0; // need fixed len for Graph constructor
+        while (synIn.hasNextLine() && hyperIn.hasNextLine()) {
+            String[] parts = synIn.readLine().split(","), synsList = parts[1].split(" ");
+            String synset = parts[1];
+            s.add(synset); // Add synset to vertex-indexed array
+
+            for (String syn : synsList) {
+                ArrayList<Integer> sList = nouns.get(syn); // List of synset ids associated with given noun
+                if (sList != null) // If noun exists, add synset id
+                    sList.add(lineNum);
+                else {
+                    ArrayList<Integer> newList = new ArrayList<>();
+                    newList.add(lineNum);
+                    nouns.put(syn, newList);
+                }
             }
+            lineNum++;
 
-            StdOut.printf("length = %d, ancestor = %d\n", length, ancestor);
+            String[] currH = hyperIn.readLine().split(","); // Read in hypernym
+            Bag<Integer> hypers = new Bag<Integer>();
+            for (int i = 1; i < currH.length; i++) // index to 1 b/c first value of hypernym line is synset id
+                hypers.add(Integer.parseInt(currH[i]));
+            adj.add(hypers);
         }
+        Digraph g = new Digraph(lineNum);
+        for (int i = 0; i < adj.size(); i++) {
+            Bag<Integer> adjBag = adj.get(i);
+            for (int vert : adjBag)
+                g.addEdge(i, vert);
+        }
+        SAP sap = new SAP(g);
+
+        // Unit test length()
+        while (testLength.hasNextLine()) {
+            String[] line = testLength.readLine().split(",");
+            int length = Integer.parseInt(line[0]);
+            ArrayList<Integer> v = nouns.get(line[1]), w = nouns.get(line[2]);
+            int testLen = sap.length(v, w);
+            assert length == testLen;
+        }
+
+        // Unit test ancestor()
+        while (testAncestor.hasNextLine()) {
+            String[] line = testAncestor.readLine().split(",");
+            String ancestor = line[0];
+            ArrayList<Integer> v = nouns.get(line[1]), w = nouns.get(line[2]);
+            assert ancestor.equals(s.get(sap.ancestor(v, w)));
+        }
+
+        // Runtime tests
+        long start = System.currentTimeMillis();
+        int cnt = 0;
+        while (System.currentTimeMillis() - start < 1000) {
+            int v = StdRandom.uniform(0, g.V()), w = StdRandom.uniform(0, g.V());
+            sap.length(v, w);
+            sap.ancestor(v, w);
+            cnt++;
+        }
+        System.out.println("Count: " + cnt);
     }
 }
--- graph/WordNet.java
+++ graph/WordNet.java
@@ -8,58 +8,79 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class WordNet {
-    private final ArrayList<String> s; // List of synsets
+    private final ArrayList<String> syns; // List of synsets
     private final HashMap<String, ArrayList<Integer>> nouns; // nouns with associated synset ids
-    private final Digraph g;
     private final SAP sap;
+    private final int n;
 
     // Constructor takes the name of the two input files
     public WordNet(String synsets, String hypernyms) {
         if (synsets == null || hypernyms == null)
             throw new IllegalArgumentException();
 
-        s = new ArrayList<>();
-        In synIn = new In(synsets), hyperIn = new In(hypernyms);
-
-        // Synset-indexed array of corresponding hypernyms
-        ArrayList<Bag<Integer>> adj = new ArrayList<>();
-
         nouns = new HashMap<>();
+        syns = new ArrayList<>();
 
-        int lineNum = 0; // need fixed len for Graph constructor
-        while (synIn.hasNextLine() && hyperIn.hasNextLine()) {
-            String[] parts = synIn.readLine().split(","), synsList = parts[1].split(" ");
-            String synset = parts[1];
-            s.add(synset); // Add synset to vertex-indexed array
+        n = parseSyns(synsets);
+        ArrayList<Bag<Integer>> hypers = parseHypers(hypernyms);
+        Digraph g = mkDigraph(n, hypers);
 
-            for (String syn : synsList) {
-                ArrayList<Integer> sList = nouns.get(syn); // List of synset ids associated with given noun
-                if (sList != null) // If noun exists, add synset id
-                    sList.add(lineNum);
-                else {
-                    ArrayList<Integer> newList = new ArrayList<>();
-                    newList.add(lineNum);
-                    nouns.put(syn, newList);
-                }
-            }
-            lineNum++;
-
-            String[] currH = hyperIn.readLine().split(","); // Read in hypernym
-            Bag<Integer> hypers = new Bag<Integer>();
-            for (int i = 1; i < currH.length; i++) // index to 1 b/c first value of hypernym line is synset id
-                hypers.add(Integer.parseInt(currH[i]));
-            adj.add(hypers);
-        }
-        // Populate digraph
-        g = new Digraph(lineNum);
-        for (int i = 0; i < adj.size(); i++) {
-            Bag<Integer> adjBag = adj.get(i);
-            for (int vert : adjBag)
-                g.addEdge(i, vert);
-        }
         if (DigraphDC.hasCycle(g))
             throw new IllegalArgumentException();
         sap = new SAP(g);
+    }
+
+    // Returns the number of created synsets for Graph constructor
+    private int parseSyns(String synsets) {
+        In synIn = new In(synsets);
+
+        int n = 0;
+        while (synIn.hasNextLine()) {
+            String[] parts = synIn.readLine().split(","), synsList = parts[1].split(" ");
+            String synset = parts[1];
+            syns.add(synset); // Add synset to vertex-indexed array
+
+            for (String syn : synsList) {
+                ArrayList<Integer> sList = nouns.get(syn); // List of synset ids associated with given noun
+                if (sList != null) // If noun exists already, add synset id
+                    sList.add(n);
+                else {
+                    ArrayList<Integer> newList = new ArrayList<>();
+                    newList.add(n);
+                    nouns.put(syn, newList);
+                }
+            }
+            n++;
+        }
+        return n;
+    }
+
+    private ArrayList<Bag<Integer>> parseHypers(String hypernyms) {
+        // Synset-indexed array of corresponding hypernyms
+        ArrayList<Bag<Integer>> hypers = new ArrayList<>();
+        In hyperIn = new In(hypernyms);
+        int cnt = 0;
+        while (hyperIn.hasNextLine()) {
+            String[] currH = hyperIn.readLine().split(","); // Read in hypernym
+            Bag<Integer> currHyper = new Bag<Integer>();
+            for (int i = 1; i < currH.length; i++) // index to 1 b/c first value of hypernym line is synset id
+                currHyper.add(Integer.parseInt(currH[i]));
+            hypers.add(currHyper);
+            cnt++;
+        }
+        if (cnt != n)
+            throw new IllegalArgumentException();
+        return hypers;
+    }
+
+    private Digraph mkDigraph(int n, ArrayList<Bag<Integer>> hypers) {
+        Digraph diG = new Digraph(n);
+        for (int i = 0; i < hypers.size(); i++) {
+            Bag<Integer> hyper = hypers.get(i);
+            for (int synset : hyper)
+                diG.addEdge(i, synset);
+        }
+        return diG;
     }
 
     // returns all WordNet nouns
@@ -85,31 +106,32 @@ public class WordNet {
     public String sap(String nounA, String nounB) {
         if (nounA == null || nounB == null || !isNoun(nounA) || !isNoun(nounB))
             throw new IllegalArgumentException();
-        return s.get(sap.ancestor(nouns.get(nounA), nouns.get(nounB)));
+        return syns.get(sap.ancestor(nouns.get(nounA), nouns.get(nounB)));
     }
 
     // do unit testing of this class
     public static void main(String[] args) {
         WordNet testNet = new WordNet(args[0], args[1]);
-        assert testNet.g.V() == testNet.s.size();
-        assert testNet.isNoun("miracle");
-        assert !testNet.isNoun("gooblegah");
+        if (args[0].contains("synsets.txt")) {
+            assert testNet.isNoun("miracle");
+            assert !testNet.isNoun("gooblegah");
 
-        String testNounA = "Lamaze_method_of_childbirth", testNounB = "Bradley_method_of_childbirth";
-        // Distance
-        int testDist = testNet.distance(testNounA, testNounB);
-        assert testDist == 2;
+            String testNounA = "Lamaze_method_of_childbirth", testNounB = "Bradley_method_of_childbirth";
+            // Distance
+            int testDist = testNet.distance(testNounA, testNounB);
+            assert testDist == 2;
 
-        String testAncestor = testNet.sap(testNounA, testNounB);
-        assert testAncestor.contains("natural_childbirth");
+            String testAncestor = testNet.sap(testNounA, testNounB);
+            assert testAncestor.contains("natural_childbirth");
 
-        String testNounC = "miracle", testNounD = "increase";
-        int testDist2 = testNet.distance(testNounC, testNounD);
+            String testNounC = "miracle", testNounD = "increase";
+            int testDist2 = testNet.distance(testNounC, testNounD);
 
-        assert testDist2 != -1;
-        assert testDist2 == 3;
+            assert testDist2 != -1;
+            assert testDist2 == 3;
 
-        String testAncestorB = testNet.sap(testNounC, testNounD);
-        assert testAncestorB.contains("happening");
+            String testAncestorB = testNet.sap(testNounC, testNounD);
+            assert testAncestorB.contains("happening");
+        }
     }
 }
